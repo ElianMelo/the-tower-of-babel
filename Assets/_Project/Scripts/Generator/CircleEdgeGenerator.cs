66bb763cdc9a1fedd6f20babb93a6aa9cf7249f3
Elian Melo <elianmelomorais@gmail.com>
2026-06-25 22:22:15 -0300

    Build floor generator

--- Assets/_Project/Scripts/Generator/CircleEdgeGenerator.cs
+++ Assets/_Project/Scripts/Generator/CircleEdgeGenerator.cs
@@ -4,10 +4,13 @@ public class CircleEdgeGenerator : MonoBehaviour
 {
     public GameObject pillarObject;
     public GameObject archObject;
+    public GameObject tileObject;
 
     public float radius;
     public float pillarAngle;
+    public float tileAngle;
     public float yOffset;
+    public float tileDecreaseRate;
 
     public float radiusFactor;
     public float heighIncrease;
@@ -19,6 +22,34 @@ public class CircleEdgeGenerator : MonoBehaviour
     private float baseHeight = 0f;
 
     void Start()
+    {
+        FillBordersForAllLevels();
+        // FillCurrentLevel(0.2f);
+    }
+
+    private void FillCurrentLevel(float floorRadius, float baseLevel)
+    {
+        float currentRadius = floorRadius;
+        float currentTileAngle = tileAngle;
+        while(currentRadius > 0)
+        {
+            currentTileAngle = tileAngle / (currentRadius / radius);
+
+            float angleRad = currentTileAngle * Mathf.Deg2Rad;
+            for (float angle = 0f; angle < Mathf.PI * 2f; angle += angleRad)
+            {
+                Vector2 point = CalculatePillarPoint(angle, currentRadius);
+                var obj = Instantiate(tileObject, new Vector3(point.x, baseLevel, point.y), Quaternion.identity);
+                Vector2 dir = (center - point).normalized;
+                float yAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+                yAngle -= 90f;
+                obj.transform.rotation = Quaternion.Euler(0, yAngle, 0);
+            }
+            currentRadius -= tileDecreaseRate;
+        }
+    }
+
+    private void FillBordersForAllLevels()
     {
         float currentRadius = radius;
         float currentPillarAngle = pillarAngle;
@@ -40,6 +71,7 @@ public class CircleEdgeGenerator : MonoBehaviour
                 objArch.transform.rotation = Quaternion.Euler(0, yAngle, 0);
                 obj.name = "Pillar " + angle;
             }
+            FillCurrentLevel(currentRadius, baseHeight + 0.2f);
 
             baseHeight += heighIncrease;
         }
