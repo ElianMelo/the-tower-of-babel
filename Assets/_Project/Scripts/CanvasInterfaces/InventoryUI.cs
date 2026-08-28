3142780c307366d0c79b63d4bcc35653a60cc89b
Elian Melo <elianmelomorais@gmail.com>
2026-08-28 18:22:36 -0300

    Implemented prefab for inventory

---
+++ Assets/_Project/Scripts/CanvasInterfaces/InventoryUI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace TowerOfBabel
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject visuals;
+
+        [SerializeField] private GameObject resourcePrefab;
+        [SerializeField] private Transform targetListResources;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Resources/Inventory/ResourceInventoryDisplay.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TowerOfBabel
+{
+    public class ResourceInventoryDisplay : MonoBehaviour
+    {
+        [SerializeField] private Image resourceIcon;
+        [SerializeField] private TMP_Text resourceName;
+        [SerializeField] private TMP_Text resourceAmount;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
